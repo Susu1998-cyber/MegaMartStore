edccfef2e1c0cd06329d1e2b30e8355498aa2f3b
SusanthInditech <s.susanth@inditechme.com>
2026-09-20 23:08:24 +0530

    vercel deployment second commit

--- frontend/src/Pages/ProductDetail.jsx
+++ frontend/src/Pages/ProductDetail.jsx
@@ -3,7 +3,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import { ShoppingCart, Minus, Plus, ArrowLeft } from "lucide-react";
 import { useDispatch } from "react-redux";
 import { getProduct } from "../services/api";
-import { addItem } from "../Redux/cartSlice";
+import { addItem } from "../Redux/CartSlice";
 import Loader from "../Components/Loader";
 
 const fallbackImage =
